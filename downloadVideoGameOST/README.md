addbe6fde634a1cbc488c486a7d9de9517a5037a
Jordan Bakker <jobafett2012@gmail.com>
2025-08-19 23:08:00 -0400

    downloading videogame soudtracks from KHInsider

---
+++ downloadVideoGameOST/README.md
@@ -0,0 +1,9 @@
+# Download Video Game Soundtracks
+
+Go to https://downloads.khinsider.com/
+
+Find the album or albums you want, copy the full URL's and put them in the $URLS array
+
+Sidenote, this site is somewhat inconsistent with naming and mp3 tags.
+
+  Filenames sometimes have double encoded URL characters in them, resulting in some oddly named downloaded files
---
+++ downloadVideoGameOST/downloadVideoGameSoundtracks.ps1
@@ -0,0 +1,22 @@
+﻿$URLS = @(
+"https://downloads.khinsider.com/game-soundtracks/album/metroid-super-metroid"
+)
+
+$BASE_PATH = "R:\MUSIC"
+
+foreach($URL in $URLS){
+    $DL_FOLDER = $(Split-Path -path "$URL" -leaf)
+    New-Item -Type Directory -Path "${BASE_PATH}\${DL_FOLDER}"
+
+    $(Invoke-WebRequest "$URL").Links | ? {$_.href -like "*.mp3" -and $_.outerText -notlike "*:*" -and $_.outerText -notlike "* MB" -and $_.outerText -ne "get_app"} | %{
+        $CONTEXT = $_.href
+        $URL = "https://downloads.khinsider.com${CONTEXT}"
+        $FILENAME = [System.Uri]::UnescapeDataString(${CONTEXT}.Replace('%25','%'))| Split-Path -Leaf
+
+        if($FILENAME.length -ge 248){
+            $FILENAME = $FILENAME.Substring(0,50)
+        }
+
+        Invoke-WebRequest ($(Invoke-WebRequest "${URL}").Links | ? {$_.href -like "*.mp3"}).href -OutFile "R:\MUSIC\${DL_FOLDER}\${FILENAME}"
+    }
+}
